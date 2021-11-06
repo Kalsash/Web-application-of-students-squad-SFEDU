6fc2b7bd19aad38a31f86589ad5937012576123d
Александр Калинин <kalsash@yandex.ru>
2021-11-06 22:54:34 +0300

    Добавьте файлы проекта.

---
+++ Web-application-of-students-squad-SFEDU/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_application_of_students_squad_SFEDU.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
