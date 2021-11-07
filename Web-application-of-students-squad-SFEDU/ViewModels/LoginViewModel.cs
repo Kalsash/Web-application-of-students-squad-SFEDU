3b2b6e4a6041e203d3e724a6f80e0d679b0ce97d
Алексадр Куземко <kuzemko@sfedu.ru>
2021-11-07 12:23:40 +0300

    Добавил класс LoginViewModel и описал его

---
+++ Web-application-of-students-squad-SFEDU/ViewModels/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web_application_of_students_squad_SFEDU.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
