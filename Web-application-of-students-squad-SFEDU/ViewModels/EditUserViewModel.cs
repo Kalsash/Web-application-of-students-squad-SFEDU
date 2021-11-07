8e3cda1ffd3fbf4bffa551c5d5b3f4baabbe8b60
Алексадр Куземко <kuzemko@sfedu.ru>
2021-11-07 17:39:36 +0300

    Добавил и описал UsersController и необходимые для него CreateUserViewModel и EditUserViewModel

---
+++ Web-application-of-students-squad-SFEDU/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_application_of_students_squad_SFEDU.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public int Year { get; set; }
+    }
+}
