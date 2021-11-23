08e144b53c8511a4621d8d859050b7eb4cf2144d
Анастасия Шагалова <shagalova.stasya@gmail.com>
2021-11-23 20:59:37 +0300

    добавила интерфейс IUserValidator

---
+++ Web-application-of-students-squad-SFEDU/ViewModels/IUserValidator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_application_of_students_squad_SFEDU.ViewModels
+{
+    public interface IUserValidator<TUser> where TUser : class
+    {
+        Task<IdentityResult> ValidateAsync(UserManager<TUser> manager, TUser user);
+    }
+}
