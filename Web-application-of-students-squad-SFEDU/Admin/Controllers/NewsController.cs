23c97e52791639838851bf453b2b243566d3084a
Александр Калинин <kalsash@yandex.ru>
2022-01-28 07:52:03 +0300

    Новости добавлены на сайт. Теперь их можно редактировать в панели администратора

---
+++ Web-application-of-students-squad-SFEDU/Admin/Controllers/NewsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Web_application_of_students_squad_SFEDU.Models;
+
+namespace Web_application_of_students_squad_SFEDU.Admin.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class NewsController : Controller
+    {
+        private readonly ArticlesRepository articlesRepository;
+        public NewsController(ArticlesRepository articlesRepository)
+        {
+            this.articlesRepository = articlesRepository;
+        }
+
+        //выбираем все записи из БД и передаем их в представление
+        public IActionResult Index(Guid id)
+        {
+            var model = articlesRepository.GetArticles();
+            if (id != default)
+            {
+                return View("Show", articlesRepository.GetArticleById(id));
+            }
+            return View(model); ;
+
+        }
+
+        public IActionResult NewsEdit(Guid id)
+        {
+            //либо создаем новую статью, либо выбираем существующую и передаем в качестве модели в представление
+            Article model = id == default ? new Article() : articlesRepository.GetArticleById(id);
+            return View(model);
+        }
+        [HttpPost] //в POST-версии метода сохраняем/обновляем запись в БД
+        public IActionResult NewsEdit(Article model)
+        {
+            if (ModelState.IsValid)
+            {
+                articlesRepository.SaveArticle(model);
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost] //т.к. удаление статьи изменяет состояние приложения, нельзя использовать метод GET
+        public IActionResult NewsDelete(Guid id)
+        {
+            articlesRepository.DeleteArticle(new Article() { Id = id });
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Web-application-of-students-squad-SFEDU/Controllers/ArticlesController.cs
+++ Web-application-of-students-squad-SFEDU/Controllers/ArticlesController.cs
@@ -16,37 +16,12 @@ namespace Web_application_of_students_squad_SFEDU.Controllers
         public IActionResult Index(Guid id)
         {
             var model = articlesRepository.GetArticles();
-                if (id != default)
-                {
-                    return View("Show", articlesRepository.GetArticleById(id));
-                }
-                return View(model);
-
-        }
-
-        public IActionResult ArticlesEdit(Guid id)
-        {
-            //либо создаем новую статью, либо выбираем существующую и передаем в качестве модели в представление
-            Article model = id == default ? new Article() : articlesRepository.GetArticleById(id);
-            return View(model);
-        }
-        [HttpPost] //в POST-версии метода сохраняем/обновляем запись в БД
-        public IActionResult ArticlesEdit(Article model)
-        {
-            if (ModelState.IsValid)
+            if (id != default)
             {
-                articlesRepository.SaveArticle(model);
-                return RedirectToAction("Index");
+                return View("Show", articlesRepository.GetArticleById(id));
             }
-
             return View(model);
-        }
 
-        [HttpPost] //т.к. удаление статьи изменяет состояние приложения, нельзя использовать метод GET
-        public IActionResult ArticlesDelete(Guid id)
-        {
-            articlesRepository.DeleteArticle(new Article() { Id = id });
-            return RedirectToAction("Index");
         }
     }
 }
