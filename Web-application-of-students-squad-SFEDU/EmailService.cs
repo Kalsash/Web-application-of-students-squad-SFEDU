4fa40a478aab359cdeb49e02fd6a7e573f2eaf73
Алексадр Куземко <kuzemko@sfedu.ru>
2022-02-18 21:40:05 +0300

    Добавил класс EmailService для отправки сообщений на почту

---
+++ Web-application-of-students-squad-SFEDU/EmailService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace Web_application_of_students_squad_SFEDU
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Штаб Студенческих Отрядов", "ShtabStudOtryadov@gmail.com"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = "Сообщение от службы поддержки ШСО";
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.gmail.com", 465, true);
+                await client.AuthenticateAsync("ShtabStudOtryadov@gmail.com", "dajwdjl39dsde");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
