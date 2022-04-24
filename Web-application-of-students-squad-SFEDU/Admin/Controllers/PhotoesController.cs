5325c2cd7600d4ce1870709f1041e592e8256d1e
Александр Калинин <kalsash@yandex.ru>
2022-04-24 12:25:38 +0300

    Оптимизация фото и фикс багов

--- Web-application-of-students-squad-SFEDU/Admin/Controllers/PhotoesController.cs
+++ Web-application-of-students-squad-SFEDU/Admin/Controllers/PhotoesController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ImageProcessor;
+using ImageProcessor.Plugins.WebP.Imaging.Formats;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
+using System.Linq;
 using Web_application_of_students_squad_SFEDU.Models;
 
 namespace Web_application_of_students_squad_SFEDU.Admin.Controllers
@@ -44,11 +47,40 @@ namespace Web_application_of_students_squad_SFEDU.Admin.Controllers
             {
                 if (titleImageFile != null)
                 {
-                    model.TitleImagePath = titleImageFile.FileName;
-                    using (var stream = new FileStream(Path.Combine(hostingEnvironment.WebRootPath, "images/gallery/", titleImageFile.FileName), FileMode.Create))
+                    // Check if valid image type (can be extended with more rigorous checks)
+                    string[] allowedImageTypes = new string[] { "image/jpeg", "image/png" };
+                    if (!allowedImageTypes.Contains(titleImageFile.ContentType.ToLower())) return View(model);
+
+                    // Prepare paths for saving images
+                    string imagesPath = Path.Combine(hostingEnvironment.WebRootPath, "images/gallery/");
+                    string webPFileName = Path.GetFileNameWithoutExtension(titleImageFile.FileName) + ".webp";
+                    string normalImagePath = Path.Combine(imagesPath, titleImageFile.FileName);
+                    string webPImagePath = Path.Combine(imagesPath, webPFileName);
+
+                    // Save the image in its original format for fallback
+                    using (var normalFileStream = new FileStream(normalImagePath, FileMode.Create))
                     {
-                        titleImageFile.CopyTo(stream);
+                        titleImageFile.CopyTo(normalFileStream);
                     }
+                    model.TitleImagePath = webPFileName;
+
+                    // Then save in WebP format
+                    using (var webPFileStream = new FileStream(webPImagePath, FileMode.Create))
+                    {
+                        using (ImageFactory imageFactory = new ImageFactory(preserveExifData: false))
+                        {
+                            imageFactory.Load(titleImageFile.OpenReadStream())
+                                        .Format(new WebPFormat())
+                                        .Quality(10)
+                                        .Save(webPFileStream);
+                        }
+                    }
+
+                    //model.TitleImagePath = titleImageFile.FileName;
+                    //using (var stream = new FileStream(Path.Combine(hostingEnvironment.WebRootPath, "images/gallery/", titleImageFile.FileName), FileMode.Create))
+                    //{
+                    //    titleImageFile.CopyTo(stream);
+                    //}
                 }
                 model.Data = DateTime.Now;
                 galeryRepository.SaveArticle(model);
