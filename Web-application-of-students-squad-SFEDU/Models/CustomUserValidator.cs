25f2acf9924dc82ef82e51a3966b9141d0826f6a
Анастасия Шагалова <shagalova.stasya@gmail.com>
2022-04-30 22:54:06 +0300

    Добавлена валидация пользователя

--- Web-application-of-students-squad-SFEDU/Models/CustomUserValidator.cs
+++ Web-application-of-students-squad-SFEDU/Models/CustomUserValidator.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Web_application_of_students_squad_SFEDU.Models
@@ -17,6 +18,84 @@ namespace Web_application_of_students_squad_SFEDU.Models
                 errors.Add(new IdentityError
                 {
                     Description = "Для регистрации можно использовать только почту sfedu.ru"
+                });
+            }
+
+            if (!Regex.IsMatch(user.Surname, "^[a-zA-ZА-яа-я]+$"))
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Фамилия должна состоять из букв!"
+                });
+            }
+            if (!Regex.IsMatch(user.Name, "^[a-zA-ZА-яа-я]+$"))
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Имя должно состоять из букв!"
+                });
+            }
+            if (!Regex.IsMatch(user.Patronymic, "^[a-zA-ZА-яа-я]+$"))
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Отчество должно состоять из букв!"
+                });
+            }
+            if (!Regex.IsMatch(user.Department, "^[a-zA-ZА-яа-я]+$"))
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Факультет должен состоять из букв!"
+                });
+            }
+            if (user.Group <1 || user.Group > 100)
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Неверный номер группы!"
+                });
+            }
+            if (user.Course < 1 || user.Course > 6)
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Неверный курс!"
+                });
+            }
+            if (user.VK.Length > 100)
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Слишком длинная ссылка!"
+                });
+            }
+            if (user.Surname.Length > 40)
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Слишком длинная Фамилия!"
+                });
+            }
+            if (user.Name.Length > 40)
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Слишком длинное Имя!"
+                });
+            }
+            if (user.Patronymic.Length > 40)
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Слишком длинное Отчество!"
+                });
+            }
+            if (user.Department.Length > 100)
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Слишком длинное название факультета!"
                 });
             }
 
